6118e4835fa9fbdd986a85bdf3891dc437027a53
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-06 15:57:10 -0600

    Section 4 - Counter Application Buttons

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,6 +2,12 @@ import React from "react";
 
 export default class Counter extends React.Component {
   render() {
-    return <div className="text-white">Counter</div>;
+    return (
+      <div className="row text-white">
+        <h1>Counter: </h1>
+        <button style={{ width: "200px" }}>+1</button>
+        <button style={{ width: "200px" }}>-1</button>
+      </div>
+    );
   }
 }
