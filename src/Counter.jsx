30557382e33577cf1cda5a9b5b21aad4de9b0a81
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-06 15:43:54 -0600

    Section 4 - Setup CounOPedia

---
+++ src/Counter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Counter extends React.Component {
+  render() {
+    return <div className="text-white">Counter</div>;
+  }
+}
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import logo from "./images/react.png";
+
+function Header() {
+  return (
+    <div className="py-2 pl-2" style={{ borderBottom: "1px solid #777" }}>
+      <img src={logo} alt="" style={{ height: "35px", verticalAlign: "top" }} />
+      <span className="h2 pt-4 m-2 text-white-50">
+        React Course - CountOpedia
+      </span>
+    </div>
+  );
+}
+
+export default Header;
