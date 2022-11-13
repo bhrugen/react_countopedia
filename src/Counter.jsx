76833e786656876fb4cdf9c02d31203f9c8fc13d
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-13 13:30:18 -0600

    Section 4 -  Game Status and Last Play

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -9,6 +9,8 @@ export default class Counter extends React.Component {
     this.handleDefence = this.handleDefence.bind(this);
     this.state = {
       count: 0,
+      gameStatus: "",
+      lastPlay: "",
     };
   }
 
@@ -18,6 +20,8 @@ export default class Counter extends React.Component {
       let newCount = previousState.count + Math.round(Math.random() * 10);
       return {
         count: newCount,
+        lastPlay: "Attack",
+        gameStatus: newCount > 10 ? "You Won!!" : previousState.gameStatus,
       };
     });
     //this.state.count = 2;
@@ -29,6 +33,8 @@ export default class Counter extends React.Component {
       let newCount = previousState.count - Math.round(Math.random() * 10);
       return {
         count: newCount,
+        lastPlay: "Defence",
+        gameStatus: newCount < -10 ? "You Lost!!" : previousState.gameStatus,
       };
     });
   };
@@ -45,6 +51,8 @@ export default class Counter extends React.Component {
     this.setState(() => {
       return {
         count: 0,
+        gameStatus: "",
+        lastPlay: "",
       };
     });
   };
@@ -53,8 +61,8 @@ export default class Counter extends React.Component {
       <div className="row text-white text-center">
         <h1>Game Score: {this.state.count} </h1>
         <p>You win at +10 points and lose at -10 points!</p>
-        <p>Last Play: </p>
-        <h3>Game Status : </h3>
+        <p>Last Play:{this.state.lastPlay} </p>
+        <h3>Game Status : {this.state.gameStatus}</h3>
         <div className="col-6 col-md-3 offset-md-3">
           <img
             style={{
@@ -67,7 +75,7 @@ export default class Counter extends React.Component {
             onClick={this.handleAttack}
           />
         </div>
-        <div className="col-6 col-md-3 offset-md-3">
+        <div className="col-6 col-md-3 ">
           <img
             style={{
               width: "100%",
